a3ab6343dacb7255aca37d0bd63ba47f72753571
Kass <hgrvzw@gmail.com>
2024-11-16 19:43:16 -0700

    Created initial add-to-routine form and view_routine page. Implementing login system next since both features require users to be logged in.

--- workoutSite/workoutApp/urls.py
+++ workoutSite/workoutApp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('exercises/', views.exerciseList, name='exerciseList'),
+    path('add-to-routine/', views.add_to_routine, name='add_to_routine'),
+    path('view-routine/', views.view_routine, name='view_routine'),
 ]
--- workoutSite/workoutApp/views.py
+++ workoutSite/workoutApp/views.py
@@ -2,6 +2,10 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 import json
 from pathlib import Path
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from workoutApp.models import Routine, Exercise
+from workoutApp.forms import AddExerciseForm
 
 # Create your views here.
 def home(request):
@@ -20,3 +24,34 @@ def exerciseList(request):
     pageObj = paginator.get_page(pageNum)
 
     return render(request, 'exerciseList.html', {'pageObj': pageObj})
+
+
+@login_required
+def add_to_routine(request):
+    if request.method == "POST":
+        form = AddExerciseForm(request.POST)
+        if form.is_valid():
+            exercise = form.save(commit=False)
+            routine, _ = Routine.objects.get_or_create(user=request.user, name="My Routine")
+            exercise.routine = routine
+            exercise.save()
+            form.save_m2m()  # Save many-to-many relationships
+            return redirect('exercise_list')
+    else:
+        # Prepopulate form with query parameters
+        initial_data = {
+            'name': request.GET.get('exercise_name', ''),
+            'primary_muscles': request.GET.get('primaryMuscles', ''),
+            'description': request.GET.get('instructions', ''),
+            'images': request.GET.get('images', ''),
+        }
+        form = AddExerciseForm(initial=initial_data)
+
+    return render(request, 'add_to_routine.html', {'form': form})
+    
+@login_required
+def view_routine(request):
+    routine = Routine.objects.filter(user=request.user).first()
+    exercises = routine.exercises.all() if routine else []
+
+    return render(request, 'view_routine.html', {'routine': routine, 'exercises': exercises})
